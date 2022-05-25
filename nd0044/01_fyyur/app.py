f9d909f15193e7f10bd24b279bd91d4a19e17a9b
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2022-05-25 13:42:24 -0700

    Bare minimum changes to launch without warning and error, connect to local db and create initial table schema.

--- nd0044/01_fyyur/app.py
+++ nd0044/01_fyyur/app.py
@@ -28,7 +28,7 @@ db = SQLAlchemy(app)
 #----------------------------------------------------------------------------#
 
 class Venue(db.Model):
-    __tablename__ = 'Venue'
+    __tablename__ = 'venues' # It doesn't like capital V in table name
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -42,7 +42,7 @@ class Venue(db.Model):
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 class Artist(db.Model):
-    __tablename__ = 'Artist'
+    __tablename__ = 'artists' # It doesn't like capital A in table name
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
