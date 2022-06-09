a41df0847b182cdd5056b74e57fb9a11953899bb
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2022-06-09 00:05:36 -0700

    Completed TODO to handle certain http requests.

--- nd0044/bookshelf/backend/flaskr/__init__.py
+++ nd0044/bookshelf/backend/flaskr/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, abort, jsonify, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy  # , or_
 from flask_cors import CORS
 import random
@@ -7,13 +7,6 @@ import random
 from models import setup_db, Book
 
 BOOKS_PER_SHELF = 8
-
-# @TODO: General Instructions
-#   - As you're creating endpoints, define them and then search for 'TODO' within the frontend to update the endpoints there.
-#     If you do not update the endpoints, the lab will not work - of no fault of your API code!
-#   - Make sure for each route that you're thinking through when to abort and with which kind of error
-#   - If you change any of the response body keys, make sure you update the frontend to correspond.
-
 
 def create_app(test_config=None):
     # create and configure the app
@@ -32,28 +25,93 @@ def create_app(test_config=None):
         )
         return response
 
-    # @TODO: Write a route that retrivies all books, paginated.
-    #         You can use the constant above to paginate by eight books.
-    #         If you decide to change the number of books per page,
-    #         update the frontend to handle additional books in the styling and pagination
-    #         Response body keys: 'success', 'books' and 'total_books'
-    # TEST: When completed, the webpage will display books including title, author, and rating shown as stars
+    @app.route('/books')
+    def get_book():
+        books = Book.query.order_by('id').all()
+        formatted_books = [ book.format() for book in books ]
+        # if page is not specified or value is not integer, default to page 1
+        page = request.args.get('page', 1, type = int)
+        # ignore negative page
+        page = page if (page >= 1) else 1
+        start = (page - 1) * BOOKS_PER_SHELF
 
-    # @TODO: Write a route that will update a single book's rating.
-    #         It should only be able to update the rating, not the entire representation
-    #         and should follow API design principles regarding method and route.
-    #         Response body keys: 'success'
-    # TEST: When completed, you will be able to click on stars to update a book's rating and it will persist after refresh
+        # requesting non-existent page
+        if (start > len(formatted_books)):
+            abort(404)
 
-    # @TODO: Write a route that will delete a single book.
-    #        Response body keys: 'success', 'deleted'(id of deleted book), 'books' and 'total_books'
-    #        Response body keys: 'success', 'books' and 'total_books'
+        return jsonify({
+            'success': True,
+            'books': formatted_books[start:(start + BOOKS_PER_SHELF)],
+            'total_books': len(books)
+        })
 
-    # TEST: When completed, you will be able to delete a single book by clicking on the trashcan.
+    @app.route('/books/<int:book_id>', methods = ['PATCH'])
+    def update_book_rating(book_id):
+        retcode = 200
+        body = request.get_json()
+        if ('rating' in body):
+            rating = body.get('rating')
+        else:
+            abort(400)
+        try:
+            book = Book.query.get(book_id)
+            if (book == None):
+                retcode = 404
+                raise Exception()
+            book.rating = rating
+            book.update()
+            return jsonify({
+                'success': True
+            })
+        except:
+            if (retcode == 404):
+                abort(retcode)
+            else:
+                abort(400)
 
-    # @TODO: Write a route that create a new book.
-    #        Response body keys: 'success', 'created'(id of created book), 'books' and 'total_books'
-    # TEST: When completed, you will be able to a new book using the form. Try doing so from the last page of books.
-    #       Your new book should show up immediately after you submit it at the end of the page.
+    @app.route('/books/<int:book_id>', methods = ['DELETE'])
+    def delete_book(book_id):
+        book = Book.query.get(book_id)
+        if (book == None):
+            abort(404)
+        book.delete()
+        #
+        # empirical data shows we don't need to return book list
+        #
+        # books = Book.query.order_by('id').all()
+        # formatted_books = [ book.format() for book in books ]
+        return jsonify({
+            'success': True,
+            'deleted': book_id,
+            # 'books': formatted_books[0:BOOKS_PER_SHELF],
+            # 'total_books': len(books)
+        })
+
+    @app.route('/books', methods = ['POST'])
+    def add_book():
+        body = request.get_json()
+        if (body is None):
+            abort(400)
+        title = body.get('title', None)
+        author = body.get('author', None)
+        rating = body.get('rating', None)
+        book = Book(title, author, rating)
+        book.insert()
+        #
+        # empirical data shows the newly added book doesn't show up on frontend immediately
+        # even in the solution provided in class. returning books doesn't seem to do anything.
+        # not returning books doesn't trigger any visual error.
+        #
+        # books = Book.query.order_by('id').all()
+        # formatted_books = [ book.format() for book in books ]
+        # last_page_start = (int(len(formatted_books) / BOOKS_PER_SHELF) - 1) * BOOKS_PER_SHELF
+        # end = last_page_start + BOOKS_PER_SHELF
+        # print(len(formatted_books))
+        return jsonify({
+            'success': True,
+            'created': book.id,
+            # 'books': formatted_books[last_page_start:end],
+            # 'total_books': len(formatted_books)
+        })
 
     return app
