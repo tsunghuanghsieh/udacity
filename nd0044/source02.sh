94a7e732f6975432b5562eec791d2e242c026062
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2022-06-13 17:17:31 -0700

    Updated dependency list and python env setup script for Chapter 2.

--- nd0044/source02.sh
+++ nd0044/source02.sh
@@ -3,7 +3,6 @@
 ENV_NAME=fsd2
 
 # source this file
-conda create --name $ENV_NAME -y python=3.7 flask flask-cors
+conda create --name $ENV_NAME -y python=3.7
 conda activate $ENV_NAME
 pip3 install -r "`dirname $0`/requirements02.txt"
-
