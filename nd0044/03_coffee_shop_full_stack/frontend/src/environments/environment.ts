cc62b35496d4cf22a77587c4c7843b79bb305252
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2022-07-08 04:27:45 -0400

    Implemented TODOs.

--- nd0044/03_coffee_shop_full_stack/frontend/src/environments/environment.ts
+++ nd0044/03_coffee_shop_full_stack/frontend/src/environments/environment.ts
@@ -1,14 +1,10 @@
-/* @TODO replace with your variables
- * ensure all variables on this page match your project
- */
-
 export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
+    url: 'nd0044ch3proj.us', // the auth0 domain prefix
+    audience: 'cafe', // the audience set for the auth0 app
     clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    callbackURL: 'http://127.0.0.1:4200', // the base url of the running ionic application.
   }
 };
