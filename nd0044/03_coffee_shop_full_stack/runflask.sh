ebac7aaad096baf63253e2b61a514691b6519db8
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2022-07-09 02:13:16 -0400

    Removed code to start and stop postgres database server. SQLite is used in this project.

--- nd0044/03_coffee_shop_full_stack/runflask.sh
+++ nd0044/03_coffee_shop_full_stack/runflask.sh
@@ -1,19 +1,4 @@
 #!/bin/zsh
 
-# check if postgres server is running
-if [ ! -f /usr/local/var/postgres/postmaster.pid ]; then
-    # start postgres server
-    pg_ctl -D /usr/local/var/postgres start -s -l "postgres_`date +%Y%m%d_%H%M%S`.log"
-else
-   echo "postgres server is already running..."
-   IS_POSTGRES_RUNNING=true
-fi
-
 export FLASK_APP=backend/src/api.py;
 flask run --reload
-
-# stop postgres server if we start it
-if expr $IS_POSTGRES_RUNNING = false > /dev/null; then
-    pg_ctl -D /usr/local/var/postgres -s stop
-fi
-
